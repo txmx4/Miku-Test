efc5f600decfb0144c075a3882c435ec19c39311
Sebi127391 <122236042+Sebi127391@users.noreply.github.com>
2023-01-11 23:55:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,28 +7,28 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['4915212908434']
-global.OwnerNumber = ['4915212908434']
-global.ownertag = ['4915212908434']
+global.Owner = ['436764242454']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.OwnerNumber = ['436764242454']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.ownertag = ['436764242454']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
 global.BotName = "Miku"
-global.packname = "Erstellt Für ✨️MAGIC✨️"
-global.author = "Für: ✨️MAGIC✨️"
-global.OwnerName = "✨️MAGIC✨️"
-global.BotSourceCode = "https://github.com/Kai0071"
-global.SupportGroupLink = "https://chat.whatsapp.com/C4UvfSXyYZoDSV3gJDyCm3"
-global.sessionName = "session"
+global.packname = "Erstellt für Sebastian"
+global.author = "Sebastian"
+global.OwnerName = "Sebastian"
+global.BotSourceCode = "https://github.com/FantoX001/Miku-MD" // -- Please don't change -- //
+global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"  // -- Please don't change -- //
+global.sessionName = "session"  // -- Don't change otherwise bot will not work -- //
 
 
 
-global.prefa = ['!']
+global.prefa = ['!'] // Prefix ---  change it to anythong you want except '@' //
 
 
-global.location = "West Berlin, Germany"
+global.location = "West-Berlin"
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
 global.websitex = "https://github.com/Kai0071"
-global.lolhuman = "✨️MAGIC✨️"
+global.lolhuman = "Sebastian"
 
 
 global.BotLogo = fs.readFileSync("./Assets/pic1.jpg")
@@ -58,19 +58,19 @@ global.ntilink = []
 
 
 global.mess = {
-    jobdone: 'Job done...',
-    useradmin: 'Sorry, only *Group Admins* can use this command *Baka*!',
-    botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
-    botowner: 'Only my *Owner* can use this command, Baka!',
-    grouponly: 'This command is only made for *Groups*, Baka!',
-    privateonly: 'This command is only made for *Private Chat*, Baka!',
-    botonly: 'Only the *Bot itself* can use this command!',
+    jobdone: 'Job erledigt...',
+    useradmin: 'Tut mir leid, nur *Gruppen-Admins* können diesen Befehl verwenden *Baka*!',
+    botadmin: 'Tut mir leid, ich kann diesen Befehl nicht ausführen, ohne ein *Admin* dieser Gruppe zu sein.',
+    botowner: 'Nur mein *Eigentümer* kann diesen Befehl verwenden, Baka!',
+    grouponly: 'Dieser Befehl gilt nur für *Groups*, Baka!',
+    privateonly: 'Dieser Befehl ist nur für *Private Chat*, Baka gedacht!',
+    botonly: 'Nur der *Bot selbst* kann diesen Befehl verwenden!',
     waiting: 'Chotto Matte...',
-    nolink: 'Please provide me *link*, Baka!',
+    nolink: 'Bitte gib mir *link*, Baka!',
     error: 'An error occurd!',
-    banned: 'You are *Banned* fron using commands!',
-    bangc: 'This Group is *Banned* from using Commands!',
-    nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
+    banned: 'Es ist Ihnen *verboten*, Befehle zu verwenden!',
+    bangc: 'Diese Gruppe ist von der Verwendung von Befehlen *gesperrt*!',
+    nonsfw: 'Sei kein perverser Baka 👉👈! Dies ist keine NSFW-aktivierte Gruppe!'
     
 }
 
